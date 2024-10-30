66a3b157a9b1014c796858dd9d3449c9408b5986
Barbiet <bogdanvinokurov2000@gmail.com>
2024-10-30 14:05:32 +0200

    Setup server, add dependencies, errorHandler and notfoundHandler.

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { env } from './utils/env.js';
+// import router from './routers/index.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
+
+const PORT = Number(env('PORT', 3000));
+export const setupServer = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use((req, res, next) => {
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    next();
+  });
+  app.get('/', (req, res) => {
+    res.json({ message: 'Hello.' });
+  });
+  // app.use(router);
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+};
