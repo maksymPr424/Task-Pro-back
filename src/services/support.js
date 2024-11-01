bbf64e6f4b36bdbc01bfaeab256c281bc49c9fdf
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-11-01 21:47:21 +0000

    Add email confirmation template and support request email functionality

--- src/services/support.js
+++ src/services/support.js
@@ -6,17 +6,49 @@ import { sendEmail } from '../utils/sendMailSupport.js';
 import { env } from '../utils/env.js';
 
 export const sendSupportEmail = async (userEmail, comment) => {
-    const supportTemplatePath = path.join(TEMPLATES_DIR, 'support-email.html');
-    const templateSource = await fs.readFile(supportTemplatePath, 'utf8');
-    const template = handlebars.compile(templateSource);
-    const html = template({ userEmail, comment });
+  const supportTemplatePath = path.join(TEMPLATES_DIR, 'support-email.html');
+  const supportTemplateSource = await fs.readFile(supportTemplatePath, 'utf8');
+  const supportTemplate = handlebars.compile(supportTemplateSource);
+  const supportHtml = supportTemplate({ userEmail, comment });
 
-    const mailOptions = {
-      from: env('SMTP_FROM'),
-      to: 'taskpro.project@gmail.com',
-      subject: 'Support Request',
-      html,
-    };
+  const userTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'user-confirmation-email.html',
+  );
+  const userTemplateSource = await fs.readFile(userTemplatePath, 'utf8');
+  const userTemplate = handlebars.compile(userTemplateSource);
+  const userHtml = userTemplate({
+    userEmail,
+    comment,
+    solutions: [
+      'Restart the application or device',
+      'Clear the application cache in device settings',
+      'Check the stability of your internet connection (Wi-Fi or mobile data)',
+      'Update the application to the latest version in the app store',
+      'Check for available storage on your device',
+      'Check if your username and password are entered correctly.',
+      'Clear the application cache in device settings.',
+      'Ensure that the app has the necessary permissions to function properly.',
+      'Log out and then log back into your account.',
+      'Try using a different browser or device if you encounter issues on the web app.',
+      'Disable any VPN or proxy that may be interfering with the connection.',
+    ],
+  });
 
-    await sendEmail(mailOptions);
+  const supportMailOptions = {
+    from: env('SMTP_FROM'),
+    to: 'taskpro.project@gmail.com',
+    subject: 'New Support Request',
+    html: supportHtml,
   };
+
+  const userMailOptions = {
+    from: env('SMTP_FROM'),
+    to: userEmail,
+    subject: 'Your Support Request Has Been Received',
+    html: userHtml,
+  };
+
+  await sendEmail(supportMailOptions);
+  await sendEmail(userMailOptions);
+};
