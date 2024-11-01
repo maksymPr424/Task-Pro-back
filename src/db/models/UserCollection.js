acb72abf638d571bacc7147ba7f5a89e5c852215
BogushF <boga1602@gmail.com>
2024-11-01 20:43:44 +0100

    board

--- src/db/models/UserCollection.js
+++ src/db/models/UserCollection.js
@@ -26,6 +26,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    activeBoard: {
+      type: String,
+    },
   },
   { timestamps: true, versionKey: false },
 );
