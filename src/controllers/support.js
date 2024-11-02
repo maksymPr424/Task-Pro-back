fcb36fcb27d3ea8cab38ad2b2160200cb8d67ad3
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-11-02 21:51:12 +0000

    Wrap sendSupportEmailController in ctrlWrapper and update email templates

--- src/controllers/support.js
+++ src/controllers/support.js
@@ -1,11 +1,11 @@
-import { sendSupportEmail } from "../services/support.js";
+import { sendSupportEmail } from '../services/support.js';
 
 export const sendSupportEmailController = async (req, res, next) => {
-    const { userEmail, comment } = req.body;
-    try {
-      await sendSupportEmail(userEmail, comment);
-      res.status(200).json({ message: 'Support request sent successfully' });
-    } catch (error) {
-      next(error);
-    }
-  };
+  const { userEmail, comment } = req.body;
+  try {
+    await sendSupportEmail(userEmail, comment);
+    res.status(200).json({ message: 'Support request sent successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/support.js
+++ src/routers/support.js
@@ -2,9 +2,14 @@ import { Router } from 'express';
 import { supportRequestSchema } from '../validation/supportValidation.js';
 import { sendSupportEmailController } from '../controllers/support.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const supportRouter = Router();
 
-supportRouter.post('/', validateBody(supportRequestSchema), sendSupportEmailController);
+supportRouter.post(
+  '/',
+  validateBody(supportRequestSchema),
+  ctrlWrapper(sendSupportEmailController),
+);
 
 export default supportRouter;
