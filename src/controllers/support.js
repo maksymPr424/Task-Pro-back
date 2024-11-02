9149b0dbbec2a052476d09303acb877c8e547ccd
Maksym Prykhodko <154377610+maksymPr424@users.noreply.github.com>
2024-11-02 23:19:47 +0100

    Update support.js

--- src/controllers/support.js
+++ src/controllers/support.js
@@ -2,10 +2,6 @@ import { sendSupportEmail } from '../services/support.js';
 
 export const sendSupportEmailController = async (req, res, next) => {
   const { userEmail, comment } = req.body;
-  try {
-    await sendSupportEmail(userEmail, comment);
-    res.status(200).json({ message: 'Support request sent successfully' });
-  } catch (error) {
-    next(error);
-  }
+  await sendSupportEmail(userEmail, comment);
+  res.status(200).json({ message: 'Support request sent successfully' });
 };
