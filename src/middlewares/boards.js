876890162532aea27a2311c8dfd18b590bb3eaa5
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2024-11-04 20:38:35 +0200

    fix board checker

--- src/middlewares/boards.js
+++ src/middlewares/boards.js
@@ -1,37 +1,18 @@
-// import createHttpError from 'http-errors';
-// import { Board } from '../db/models/board.js';
-
-// export const findBoardById = async (req, res, next) => {
-//   try {
-//     const { boardId } = req.query;
-//     const board = await Board.findById(boardId);
-
-//     if (!board) {
-//       return next(createHttpError(404, `Board with id ${boardId} not found`));
-//     }
-
-//     req.board = board;
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 import createHttpError from 'http-errors';
 import { Board } from '../db/models/board.js';
 
 export const findBoardById = async (req, res, next) => {
   try {
-    const boardId = req.query.boardId; // Предположим, что boardId передается через query-параметры
+    const { boardId } = req.query;
     const board = await Board.findById(boardId);
 
     if (!board) {
       return next(createHttpError(404, `Board with id ${boardId} not found`));
     }
 
-    req.board = board; // Делаем доску доступной в запросе
-    next(); // Передаем управление следующему middleware или контроллеру
+    req.board = board;
+    next();
   } catch (error) {
-    next(error); // Обработка ошибок
+    next(error);
   }
 };
