43a0eb89dbfc92aeaadefadafead71c2b4ffed46
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-11-04 17:03:34 +0000

    Add schema for support request, including validation for email format and lengths of userEmail and comment

--- src/validation/supportValidation.js
+++ src/validation/supportValidation.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const supportRequestSchema = Joi.object({
-  userEmail: Joi.string().email().required(),
-  comment: Joi.string().min(10).required(),
+  userEmail: Joi.string().email().min(5).max(254).required(),
+  comment: Joi.string().min(10).max(500).required(),
 });
