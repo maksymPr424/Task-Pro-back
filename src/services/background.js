4cd226f5dc408cefeaf691c70bd03ac50ab4a731
Barbiet <bogdanvinokurov2000@gmail.com>
2024-11-09 17:08:16 +0200

    Fix All backgroundRouter

--- src/services/background.js
+++ src/services/background.js
@@ -1,10 +1,10 @@
 import { Background } from '../db/models/background.js';
-
 import createHttpError from 'http-errors';
+
 export const getBackgroundById = async (name) => {
-  const backgroung = await Background.findOne({ name });
-  if (!backgroung) {
+  const background = await Background.findOne({ name });
+  if (!background) {
     throw createHttpError(404, `Backgroung with ${name} not found`);
   }
-  return backgroung;
+  return background;
 };
