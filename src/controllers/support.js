7da32a0a15044e24cfcdea2d1291509424250bdc
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-11-09 16:47:58 +0000

    Refactor: optimize email validation and controller logic

--- src/controllers/support.js
+++ src/controllers/support.js
@@ -1,18 +1,7 @@
 import { sendSupportEmail } from '../services/support.js';
-import { verifyEmailEmailable } from '../services/emailVerification.js';
 
-export const sendSupportEmailController = async (req, res, next) => {
+export const sendSupportEmailController = async (req, res) => {
   const { userEmail, comment } = req.body;
-  try {
-    const verificationResponse = await verifyEmailEmailable(userEmail);
-
-    if (verificationResponse.state === 'deliverable') {
-      await sendSupportEmail(userEmail, comment);
-      res.status(200).json({ message: 'Support request sent successfully' });
-    } else {
-      res.status(400).json({ message: 'Invalid email address' });
-    }
-  } catch (error) {
-    next(error);
-  }
+  await sendSupportEmail(userEmail, comment);
+  res.status(200).json({ message: 'Support request sent successfully' });
 };
--- src/middlewares/validateEmailWithEmailable.js
+++ src/middlewares/validateEmailWithEmailable.js
@@ -5,18 +5,10 @@ const validateEmailWithEmailable = async (req, res, next) => {
   const { userEmail } = req.body;
 
   try {
-    const verificationResponse = await verifyEmailEmailable(userEmail);
-
-    if (verificationResponse.state === 'deliverable') {
-      next();
-    } else {
-      return next(
-        new createHttpError(422, 'Invalid or non-existent email address'),
-      );
-    }
-  } catch (error) {
-    console.error('Error verifying email address with Emailable:', error);
-    return next(new createHttpError(500, 'Error verifying email address'));
+    await verifyEmailEmailable(userEmail);
+    next();
+  } catch {
+    next(createHttpError(422, 'Invalid or non-existent email address'));
   }
 };
 
