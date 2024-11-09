ee96e8ac6e3b443c04dccd3a6fd81c63668c8480
Barbiet <bogdanvinokurov2000@gmail.com>
2024-11-09 16:09:42 +0200

    Fix BackgroundSchema

--- src/db/models/background.js
+++ src/db/models/background.js
@@ -1,6 +1,15 @@
 import { model, Schema } from 'mongoose';
-const BackgroundSchema = new Schema({
-  name: String,
-  url: String,
+
+const ImageUrlsSchema = new Schema({
+  mobile: { type: String, required: true },
+  tablet: { type: String, required: true },
+  desktop: { type: String, required: true },
 });
-export const Background = model('backgrounds', BackgroundSchema);
+
+const BackgroundSchema = new Schema({
+  name: { type: String, required: true },
+  title: { type: String, required: true },
+  urls: { type: ImageUrlsSchema, required: true },
+});
+
+export const Background = model('background', BackgroundSchema);
